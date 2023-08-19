e67755c04b0f28cda597628def21c9e8138a55b5
Carlos Santos <gotgoodjob@gmail.com>
2023-08-19 15:07:05 +0800

    Initial Commit

---
+++ Learning-GH2/Learning_GH2App.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GH2App.swift
+//  Learning-GH2
+//
+//  Created by Carlos Dos santos on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GH2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
