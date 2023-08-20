58d4f636a0cdf1c60e24f29d82ea2ee847a6650e
CarlosS9400 <100578104+CarlosS9400@users.noreply.github.com>
2023-08-20 08:16:22 +0800

    update ContentView.swift to what's up

--- Learning-GH2/ContentView.swift
+++ Learning-GH2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world world!")
+            Text("What's up, world world!")
         }
         .padding()
     }
